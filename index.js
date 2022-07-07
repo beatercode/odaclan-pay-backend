a0652192613841cd352b8e66115d979db1e394bd
Luca Minoi <beater@MBP-di-Luca.wind3.hub>
2022-07-07 23:11:27 +0200

    ao

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ const clearEmpytLink = require("./helper/clearEmpytLink")
 const hostname = '127.0.0.1';
 const port = 3010;
 
-var task_clearEmptyLink = cron.schedule('*/30 * * * *', () => {
+var task_clearEmptyLink = cron.schedule('*/30 * * * *', async () => {
     await clearEmpytLink()
 });
-var task_checkPendingPayments = cron.schedule('*/5 * * * *', () => {
+var task_checkPendingPayments = cron.schedule('*/5 * * * *', async () => {
     let _status = "pending";
     let payments = await Payment.find({ status: _status })
 
-    payments.forEach(payment => {
+    payments.forEach(async payment => {
         await checkPayment(payment)
     });
 });
 
-var task_checkSuccessPayments = cron.schedule('*/5 * * * *', () => {
+var task_checkSuccessPayments = cron.schedule('*/5 * * * *', async () => {
     let _status = "success";
     let payments = await Payment.find({ status: _status })
 
-    payments.forEach(payment => {
+    payments.forEach(async payment => {
         await checkAndSendMail(payment)
     });
 });
