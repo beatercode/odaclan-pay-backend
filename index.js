cefcfe23c95049478d785d35f498388579d60ea6
Luca Minoi <beater@MBP-di-Luca.wind3.hub>
2022-07-07 23:35:34 +0200

    aa

--- index.js
+++ index.js
@@ -1,61 +1,8 @@
 const http = require('http');
 var cron = require('node-cron');
-const Payment = require("./models/Payment");
-const checkAndSendMail = require("./helper/mail")
-const clearEmpytLink = require("./helper/clearEmpytLink")
 
 const hostname = '127.0.0.1';
 const port = 3010;
-
-var task_clearEmptyLink = cron.schedule('*/30 * * * *', async () => {
-    console.log("RUN [task_clearEmptyLink]")
-    try {
-        await clearEmpytLink()
-    } catch (err) {
-        console.log(err)
-    }
-
-    console.log("END [task_clearEmptyLink]")
-});
-var task_checkPendingPayments = cron.schedule('*/2 * * * *', async () => {
-    console.log("RUN [task_checkPendingPayments]")
-    try {
-        let _status = "pending";
-        let payments = await Payment.find({ status: _status })
-        console.log("payments pending:")
-        console.log(payments)
-
-        console.log("FOR [task_checkPendingPayments] N[" + payments.length + "] payments")
-        payments.forEach(async payment => {
-            await checkPayment(payment)
-        });
-    } catch (err) {
-        console.log(err)
-    }
-    console.log("END [task_checkPendingPayments]")
-});
-
-var task_checkSuccessPayments = cron.schedule('*/2 * * * *', async () => {
-    console.log("RUN [task_checkSuccessPayments]")
-    try {
-        let _status = "success";
-        let payments = await Payment.find({ status: _status })
-        console.log("payments success:")
-        console.log(payments)
-
-        console.log("FOR [task_checkSuccessPayments] N[" + payments.length + "] payments")
-        payments.forEach(async payment => {
-            await checkAndSendMail(payment)
-        });
-    } catch (err) {
-        console.log(err)
-    }
-    console.log("END [task_checkSuccessPayments]")
-});
-
-task_clearEmptyLink.start();
-task_checkPendingPayments.start();
-task_checkSuccessPayments.start();
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const Router = require("./routes/routes")
+var cron = require('node-cron');
 const cors = require('cors')
 const port = 3003
+const Payment = require("./models/Payment");
+const checkPayment = require("./helper/checkPayments")
+const checkAndSendMail = require("./helper/mail")
+const clearEmpytLink = require("./helper/clearEmpytLink")
 
 const app = express();
 
@@ -20,6 +25,57 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
     console.log("Connected successfully");
+
+    var task_clearEmptyLink = cron.schedule('*/30 * * * *', async () => {
+        console.log("RUN [task_clearEmptyLink]")
+        try {
+            await clearEmpytLink()
+        } catch (err) {
+            console.log(err)
+        }
+
+        console.log("END [task_clearEmptyLink]")
+    });
+
+    var task_checkPendingPayments = cron.schedule('*/2 * * * *', async () => {
+        console.log("RUN [task_checkPendingPayments]")
+        try {
+            let _status = "pending";
+            let payments = await Payment.find({ status: _status })
+            //console.log("payments pending:")
+            //console.log(payments)
+
+            console.log("FOR [task_checkPendingPayments] N[" + payments.length + "] payments")
+            payments.forEach(async payment => {
+                await checkPayment(payment)
+            });
+        } catch (err) {
+            console.log(err)
+        }
+        console.log("END [task_checkPendingPayments]")
+    });
+
+    var task_checkSuccessPayments = cron.schedule('*/2 * * * *', async () => {
+        console.log("RUN [task_checkSuccessPayments]")
+        try {
+            let _status = "success";
+            let payments = await Payment.find({ status: _status })
+            //console.log("payments success:")
+            //console.log(payments)
+
+            console.log("FOR [task_checkSuccessPayments] N[" + payments.length + "] payments")
+            payments.forEach(async payment => {
+                await checkAndSendMail(payment)
+            });
+        } catch (err) {
+            console.log(err)
+        }
+        console.log("END [task_checkSuccessPayments]")
+    });
+
+    task_clearEmptyLink.start();
+    task_checkPendingPayments.start();
+    task_checkSuccessPayments.start();
 });
 
 app.use(Router);
